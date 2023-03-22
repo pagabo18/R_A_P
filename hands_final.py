161855f2019d4a97374dcdf75eb309d433cbec71
pagabo18 <pagabo18@hotmail.com>
2023-03-22 09:10:55 -0600

    Test/multihandeness Interface

--- hands_final.py
+++ hands_final.py
@@ -1,10 +1,11 @@
-
+from abc import ABC, abstractmethod
 import mediapipe as mp
 import cv2
 import numpy as np
 from math import sqrt
 import open3d as o3d
 import mediapipe as mp
+
 
 
 
@@ -51,13 +52,6 @@ class hand_detector:
     def Start(self):
         pass
     
-    def same_hand_detected(self):
-        if  self.results.multi_handedness:
-                self.totalHands = len( self.results.multi_handedness)
-                if (self.totalHands == 2):
-                    if ( self.results.multi_handedness[0].classification[0].label ==  self.results.multi_handedness[1].classification[0].label):
-                        self.totalHands = 1
-    
     def index(self):
         indexTip =  self.results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
         indexTipXY = mp_drawing._normalized_to_pixel_coordinates(indexTip.x, indexTip.y,  self.frameWidth,  self.frameHeight)
@@ -67,6 +61,13 @@ class hand_detector:
         thumbTip =  self.results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.THUMB_TIP]
         thumbTipXY = mp_drawing._normalized_to_pixel_coordinates( thumbTip.x, thumbTip.y,  self.frameWidth,  self.frameHeight)
         return thumbTipXY
+    
+    def same_hand_detected(self):
+        if  self.results.multi_handedness:
+                self.totalHands = len( self.results.multi_handedness)
+                if (self.totalHands == 2):
+                    if ( self.results.multi_handedness[0].classification[0].label ==  self.results.multi_handedness[1].classification[0].label):
+                        self.totalHands = 1
     
     def multi_handedness(self,viewer):
         if  self.results.multi_hand_landmarks:
@@ -173,105 +174,85 @@ class hand_detector:
                         if self.initialpose == False:
                             self.initialpose = True
                             print("Regresando a posición Inicial")
-                            viewer.vis_general_reset()  
-                             
-# def multi_handedness(self, viewer):
-#     if self.results.multi_hand_landmarks:
-#         if self.initialpose:
-#             self.initialpose = False   
-#         if (self.totalHands == 1):
-#             self.handle_single_hand(viewer)
-#         elif (self.totalHands == 2):
-#             self.handle_two_hands(viewer)
+                            viewer.vis_general_reset()    
+    
+#     class MultiHandeness(ABC):
+#     def __init__(self):
+#         self.totalHands = 0
+        
+#     @abstractmethod
+#     def same_hand_detected(self):
+#         pass
+        
+#     @abstractmethod
+#     def multi_handedness(self,viewer):
+#         pass
 
-# def handle_single_hand(self, viewer):
-#     hand = self.results.multi_hand_landmarks[0]
-#     normalizedLandmark = hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-#     pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, self.frameWidth, self.frameHeight)
 
-#     indexTip = hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-#     indexTipXY = mp_drawing._normalized_to_pixel_coordinates(indexTip.x, indexTip.y, self.frameWidth, self.frameHeight)
+# class SingleHand(MultiHandeness):
+#     def __init__(self):
+#         super().__init__()
+    
+#     def same_hand_detected(self):
+#         if self.results.multi_handedness:
+#             self.totalHands = len(self.results.multi_handedness)
+#             if (self.totalHands == 2):
+#                 if (self.results.multi_handedness[0].classification[0].label == self.results.multi_handedness[1].classification[0].label):
+#                     self.totalHands = 1
+                    
+#     def multi_handedness(self, viewer):
+#         if self.results.multi_hand_landmarks:
+#             if self.initialpose:
+#                 self.initialpose = False   
+#             if (self.totalHands == 1):
+#                 for num, hand in enumerate(self.results.multi_hand_landmarks):
+#                     normalizedLandmark = self.results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+#                     pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, self.frameWidth, self.frameHeight)
 
-#     thumbTip = hand.landmark[mp_hands.HandLandmark.THUMB_TIP]
-#     thumbTipXY = mp_drawing._normalized_to_pixel_coordinates(thumbTip.x, thumbTip.y, self.frameWidth, self.frameHeight)
+#                     indexTipXY = self.index()
 
-#     if pixelCoordinatesLandmark and indexTipXY and thumbTipXY is not None:
-#         indexXY = (indexTipXY[0], indexTipXY[1])
-#         thumbXY = (thumbTipXY[0], thumbTipXY[1])
-#         cv2.circle(self.image, indexXY, 10, (255, 0, 0), 2)
-#         cv2.circle(self.image, thumbXY, 10, (255, 0, 0), 2)
-#         dist = hand_detector.calc_distance(indexXY, thumbXY)
-#         if (dist < 50):
-#             self.handle_rotation(viewer, indexTipXY, thumbTipXY)
-#         else:
-#             self.moveX = 0
-#             self.moveY = 0
+#                     thumbTipXY = self.thumb()
 
-#     mp_drawing.draw_landmarks(self.image, hand, mp_hands.HAND_CONNECTIONS, mp_drawing.DrawingSpec(
-#         color=(121, 22, 76), thickness=2, circle_radius=4), mp_drawing.DrawingSpec(
-#         color=(250, 44, 250), thickness=2, circle_radius=2))
+#                     if pixelCoordinatesLandmark and indexTipXY and thumbTipXY is not None:
+#                         indexXY = (indexTipXY[0], indexTipXY[1])
+#                         thumbXY = (thumbTipXY[0], thumbTipXY[1])
+#                         cv2.circle(self.image, indexXY, 10, (255, 0, 0), 2)
+#                         cv2.circle(self.image, thumbXY, 10, (255, 0, 0), 2)
+#                         dist = hand_detector.calc_distance(indexXY, thumbXY)
+#                         if (dist < 50):
+#                             netX = round((indexTipXY[0]+thumbTipXY[0])/2)
+#                             netY = round((indexTipXY[1]+thumbTipXY[1])/2)
+#                             cv2.circle(self.image, (netX, netY),10, (0, 255, 0), 2)
+#                             deltaX = self.moveX - netX
+#                             self.moveX = netX
+#                             deltaY = self.moveY - netY
+#                             self.moveY = netY
+#                             if abs(deltaX) > 40 or abs(deltaY) > 40:
+#                                 print("Max reached: " + str(deltaX)+","+str(deltaY))
+#                             else:
+#                                 print(str(deltaX)+","+str(deltaY))
+#                                 viewer.vis_rotate(deltaX, deltaY)
+#                         else:
+#                             self.moveX = 0
+#                             self.moveY = 0
 
-# def handle_two_hands(self, viewer):
-#     handX = [0, 0]
-#     handY = [0, 0]
-#     isHands = [False, False]
+#                     mp_drawing.draw_landmarks(self.image, hand, mp_hands.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4), mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
 
-#     for num, hand in enumerate(self.results.multi_hand_landmarks):
-
-#         indexTip = hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-#         indexTipXY = mp_drawing._normalized_to_pixel_coordinates(indexTip.x, indexTip.y, self.frameWidth, self.frameHeight)
-
-#         thumbTip = hand.landmark[mp_hands.HandLandmark.THUMB_TIP]
-#         thumbTipXY = mp_drawing._normalized_to_pixel_coordinates(thumbTip.x, thumbTip.y, self.frameWidth, self.frameHeight)
-
-#         if indexTip and indexTipXY and thumbTipXY is not None:
-#             indexXY = (indexTipXY[0], indexTipXY[1])
-#             thumbXY = (thumbTipXY[0], thumbTipXY[1])
-#             cv2.circle(self.image, indexXY, 10, (255, 0, 0), 2)
-#             cv2.circle(self.image, thumbXY, 10, (255, 0, 0), 2)
-#             dist = hand_detector.calc_distance(indexXY, thumbXY)
-#             if (dist < 50):
-#                 netX = round((indexTipXY[0]+thumbTipXY[0])/2)
-#                 netY = round((indexTipXY[1]+thumbTipXY[1])/2)
-#                 handX[num] = netX
-#                 handY[num] = netY
-#                 isHands[num] = True
-
-#                 mp_drawing.draw_landmarks( self.image, hand, mp_hands.HAND_CONNECTIONS,
-#                 mp_drawing.DrawingSpec(
-#                 color=(121, 22, 76), thickness=2, circle_radius=4),
-#                 mp_drawing.DrawingSpec(
-#                 color=(250, 44, 250), thickness=2, circle_radius=2)
-#                 )
-
-#                 if (isHands[0] and isHands[1]):
-#                     distpar = hand_detector.calc_distance(
-#                         (handX[0], handY[0]), (handX[1], handY[1]))
-#                     if (self.newZ):
-#                         self.newZ = False
-#                         self.moveZ = distpar
-#                         self.refZ = distpar
-#                     netX = round((handX[0]+handX[1])/2)
-#                     netY = round((handY[0]+handY[1])/2)
-#                     deltaZ = (distpar - self.moveZ)/self.refZ
-#                     if (deltaZ < abs(1)):
-#                         self.absZ = self.absZ - deltaZ
-#                         if (self.absZ > 2.0):
-#                             self.absZ = 2.0
-#                         elif (self.absZ < 0.5):
-#                             self.absZ = 0.5
-#                         self.moveZ = distpar
-#                         print(self.absZ)
-#                         cv2.circle( self.image, (netX, netY),10, (0, 0, 255), 2)
-#                         viewer.vis_zoom(self.absZ)
-
-#                     elif (not isHands[0] and not isHands[1]):
-#                         self.newZ = True 
-#                     else:
-#                         if self.initialpose == False:
-#                             self.initialpose = True
-#                             print("Regresando a posición Inicial")
-#                             viewer.vis_general_reset()  
+    
+# class DoubleHands(MultiHandeness):
+#     def __init__(self):
+#         super().__init__()
+#         self.newZ = True
+        
+#     def same_hand_detected(self):
+#         if self.results.multi_handedness:
+#             self.totalHands = len(self.results.multi_handedness)
+#             if (self.totalHands == 2):
+#                 if (self.results.multi_handedness[0].classification[0].
+    
+    
+    
+    
 class capture:
     def __init__(self, makeoptimize=False):
         if makeoptimize:
@@ -327,6 +308,7 @@ class ObjectViewer:
                 self.zoomcounter = 0
             self.vis.poll_events()
             self.vis.update_renderer()
+            
     def make_fullscreen(self,makefullscreen=False):
         if not makefullscreen:
                 cv2.imshow('Hand Tracking',  hands_detection.image)
